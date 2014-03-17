225be7439f7ad5df941a9e9076d0044a511f87a5
Martin Kiss <martin.kiss@me.com>
2014-03-17 18:38:56 +0100

    Display Locale calendar and quotation symbols

--- Sources/Tool Locale/TBXLocaleToolDesign.m
+++ Sources/Tool Locale/TBXLocaleToolDesign.m
@@ -30,7 +30,7 @@
         self->_workingLocale = [NSLocale currentLocale];
         
         [[OCAProperty(self, workingLocale, NSLocale) transformValues:
-         [OCATransformer objectForKey:NSLocaleCountryCode],
+          [OCATransformer objectForKey:NSLocaleCountryCode],
           nil] connectTo:OCAProperty(self, workingRepresentation, NSString)];
         
         [[OCAProperty(self, workingLocale, NSLocale) transformValues:
@@ -43,12 +43,17 @@
                                    @"Country",
                                    @"Script",
                                    @"Variant",
+                                   //TODO: Exemplar Characters
+                                   @"Calendar",
+                                   //TODO: Collation Identifier
                                    @"Currency",
                                    @"Currency Code",
                                    @"Currency Symbol",
                                    @"Measurement System",
                                    @"Decimal Separator",
                                    @"Grouping Separator",
+                                   @"Quotation",
+                                   @"Alternate Quotation",
                                    ];
         self->_componentCount = self.componentTitles.count;
         [[OCAProperty(self, workingLocale, NSLocale) transformValues:
@@ -58,17 +63,53 @@
                                         [self.class transformLocaleToDisplayNameForKey:NSLocaleCountryCode],
                                         [self.class transformLocaleToDisplayNameForKey:NSLocaleScriptCode],
                                         [self.class transformLocaleToDisplayNameForKey:NSLocaleVariantCode],
+                                        [OCATransformer sequence:
+                                         @[
+                                           [OCATransformer objectForKey:NSLocaleCalendar],
+                                           [OCATransformer access:OCAKeyPath(NSCalendar, calendarIdentifier, NSString)],
+                                           [OCATransformer map:@{
+                                                                 NSGregorianCalendar: @"Gregorian",
+                                                                 NSBuddhistCalendar: @"Buddhist",
+                                                                 NSChineseCalendar: @"Chinese",
+                                                                 NSHebrewCalendar: @"Hebrew",
+                                                                 NSIslamicCalendar: @"Islamic",
+                                                                 NSIslamicCivilCalendar: @"Islamic Civil",
+                                                                 NSJapaneseCalendar: @"Japanese",
+                                                                 NSRepublicOfChinaCalendar: @"Taiwan",
+                                                                 NSPersianCalendar: @"Persian",
+                                                                 NSIndianCalendar: @"Indian",
+                                                                 NSISO8601Calendar: @"ISO-8601",
+                                                                 NSNull.null : @"Unknown",
+                                                                 }],
+                                           ]],
                                         [self.class transformLocaleToDisplayNameForKey:NSLocaleCurrencyCode],
                                         [OCATransformer objectForKey:NSLocaleCurrencyCode],
                                         [OCATransformer objectForKey:NSLocaleCurrencySymbol],
                                         [OCATransformer objectForKey:NSLocaleMeasurementSystem],
                                         [OCATransformer objectForKey:NSLocaleDecimalSeparator],
-                                        [OCATransformer sequence:@[
-                                                                   [OCATransformer objectForKey:NSLocaleGroupingSeparator],
-                                                                   [OCATransformer if:[OCAPredicate isIn:@"  "] // SPACE, NO-BREAK SPACE
-                                                                    then:[OCATransformer replaceWith:@"(space)"]
-                                                                    else:[OCATransformer pass]],
-                                                                   ]],
+                                        [OCATransformer sequence:
+                                         @[
+                                           [OCATransformer objectForKey:NSLocaleGroupingSeparator],
+                                           [OCATransformer if:[OCAPredicate isIn:@"  "] // SPACE, NO-BREAK SPACE
+                                            then:[OCATransformer replaceWith:@"(space)"]
+                                            else:[OCATransformer pass]],
+                                           ]],
+                                        [OCATransformer sequence:
+                                         @[
+                                           [OCATransformer branchArray:@[
+                                                                         [OCATransformer objectForKey:NSLocaleQuotationBeginDelimiterKey],
+                                                                         [OCATransformer objectForKey:NSLocaleQuotationEndDelimiterKey],
+                                                                         ]],
+                                           [OCATransformer joinWithString:@" "],
+                                           ]],
+                                        [OCATransformer sequence:
+                                         @[
+                                           [OCATransformer branchArray:@[
+                                                                         [OCATransformer objectForKey:NSLocaleAlternateQuotationBeginDelimiterKey],
+                                                                         [OCATransformer objectForKey:NSLocaleAlternateQuotationEndDelimiterKey],
+                                                                         ]],
+                                           [OCATransformer joinWithString:@" "],
+                                           ]],
                                         ]],
           nil] connectTo:OCAProperty(self, componentValues, NSArray)];
     }
