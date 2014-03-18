104288ecd0008da4d48e0751cce7d1ff69fa7bd3
Martin Kiss <martin.kiss@me.com>
2014-03-18 08:54:10 +0100

    Create separate Design object for Lcoale

---
+++ Sources/Tool Locale/TBXLocaleDesign.h
@@ -0,0 +1,60 @@
+//
+//  TBXLocaleDesign.h
+//  Toolbox
+//
+//  Created by Martin Kiss on 18.3.14.
+//  Copyright (c) 2014 Triceratops Software s.r.o. All rights reserved.
+//
+
+@import Foundation.NSObject;
+@import Foundation.NSLocale;
+
+
+
+
+
+@interface TBXLocaleDesign : NSObject
+
+
+@property (atomic, readwrite, strong) NSLocale *locale;
+@property (atomic, readwrite, strong) NSLocale *displayLocale;
+
+@property (atomic, readonly, copy) NSString *identifier;
+@property (atomic, readonly, assign) NSUInteger windowsCode;
+@property (atomic, readonly, copy) NSString *title;
+
+@property (atomic, readonly, copy) NSString *languageCode;
+@property (atomic, readonly, copy) NSString *languageName;
+@property (atomic, readonly, copy) NSString *countryCode;
+@property (atomic, readonly, copy) NSString *countryName;
+@property (atomic, readonly, copy) NSString *variantCode;
+
+@property (atomic, readonly, copy) NSString *scriptCode;
+@property (atomic, readonly, assign) NSLocaleLanguageDirection lineDirection;
+@property (atomic, readonly, assign) NSLocaleLanguageDirection characterDirection;
+@property (atomic, readonly, copy) NSString *exemplarCharacters;
+
+@property (atomic, readonly, copy) NSString *calendarName;
+@property (atomic, readonly, copy) NSString *shortDate;
+@property (atomic, readonly, copy) NSString *mediumDate;
+@property (atomic, readonly, copy) NSString *longDate;
+@property (atomic, readonly, copy) NSString *fullDate;
+
+@property (atomic, readonly, copy) NSString *measurementSystem;
+@property (atomic, readonly, copy) NSString *decimalSeparator;
+@property (atomic, readonly, copy) NSString *groupingSeparator;
+
+@property (atomic, readonly, copy) NSString *currencyCode;
+@property (atomic, readonly, copy) NSString *currencyName;
+@property (atomic, readonly, copy) NSString *currencySymbol;
+@property (atomic, readonly, copy) NSString *currencyExample;
+
+@property (atomic, readonly, copy) NSString *quotationSymbols;
+@property (atomic, readonly, copy) NSString *quotationExample;
+@property (atomic, readonly, copy) NSString *alternateQuotationSymbols;
+@property (atomic, readonly, copy) NSString *alternateQuotationExample;
+
+
+@end
+
+
---
+++ Sources/Tool Locale/TBXLocaleDesign.m
@@ -0,0 +1,113 @@
+//
+//  TBXLocaleDesign.m
+//  Toolbox
+//
+//  Created by Martin Kiss on 18.3.14.
+//  Copyright (c) 2014 Triceratops Software s.r.o. All rights reserved.
+//
+
+#import "TBXLocaleDesign.h"
+
+
+
+
+
+@implementation TBXLocaleDesign
+
+
+
+
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self->_locale = [NSLocale currentLocale];
+        self->_displayLocale = [NSLocale currentLocale];
+        
+        OCAProperty *locale = OCAProperty(self, locale, NSLocale);
+        OCAProducer *localeForDisplay = [locale dependOn:OCAProperty(self, displayLocale, NSLocale), nil];
+        
+        
+        
+        [[locale transformValues:
+          [OCATransformer objectForKey:NSLocaleIdentifier],
+          nil] connectTo:OCAProperty(self, identifier, NSString)];
+        
+        [[OCAProperty(self, identifier, NSString) transformValues:
+          [OCATransformer fromClass:[NSString class] toClass:[NSNumber class]
+                          asymetric:^NSNumber *(NSString *input) {
+                              uint32_t code = [NSLocale windowsLocaleCodeFromLocaleIdentifier:input];
+                              return @(code);
+                          }],
+          nil] connectTo:OCAProperty(self, windowsCode, NSUInteger)];
+        
+        [[localeForDisplay transformValues:
+          [self transformLocaleToDisplayNameForKey:NSLocaleIdentifier],
+          nil] connectTo:OCAProperty(self, title, NSString)];
+        
+        
+        
+        [[locale transformValues:
+          [OCATransformer objectForKey:NSLocaleLanguageCode],
+          nil] connectTo:OCAProperty(self, languageCode, NSString)];
+        
+        [[localeForDisplay transformValues:
+          [self transformLocaleToDisplayNameForKey:NSLocaleLanguageCode],
+          nil] connectTo:OCAProperty(self, languageName, NSString)];
+        
+        [[locale transformValues:
+          [OCATransformer objectForKey:NSLocaleCountryCode],
+          nil] connectTo:OCAProperty(self, countryCode, NSString)];
+        
+        [[localeForDisplay transformValues:
+          [self transformLocaleToDisplayNameForKey:NSLocaleCountryCode],
+          nil] connectTo:OCAProperty(self, countryName, NSString)];
+        
+        [[locale transformValues:
+          [OCATransformer objectForKey:NSLocaleVariantCode],
+          nil] connectTo:OCAProperty(self, variantCode, NSString)];
+        
+        
+        
+        [[locale transformValues:
+          [OCATransformer objectForKey:NSLocaleScriptCode],
+          nil] connectTo:OCAProperty(self, scriptCode, NSString)];
+        
+        [[OCAProperty(self, languageCode, NSString) transformValues:
+          [OCATransformer fromClass:[NSString class] toClass:[NSNumber class]
+                          asymetric:^NSNumber *(NSString *input) {
+                              NSLocaleLanguageDirection direction = [NSLocale lineDirectionForLanguage:input];
+                              return @(direction);
+                          }],
+          nil] connectTo:OCAProperty(self, lineDirection, NSUInteger)];
+        
+        [[OCAProperty(self, languageCode, NSString) transformValues:
+          [OCATransformer fromClass:[NSString class] toClass:[NSNumber class]
+                          asymetric:^NSNumber *(NSString *input) {
+                              NSLocaleLanguageDirection direction = [NSLocale characterDirectionForLanguage:input];
+                              return @(direction);
+                          }],
+          nil] connectTo:OCAProperty(self, characterDirection, NSUInteger)];
+        
+        self->_exemplarCharacters = @"ABC"; //TODO: String from Character set
+    }
+    return self;
+}
+
+
+- (NSValueTransformer *)transformLocaleToDisplayNameForKey:(NSString *)key {
+    return [OCATransformer fromClass:[NSLocale class] toClass:[NSString class]
+                           asymetric:^NSString *(NSLocale *input) {
+                               
+                               id value = [input objectForKey:key];
+                               return [self.displayLocale displayNameForKey:key value:value];
+                           }];
+}
+
+
+
+
+
+@end
+
+
